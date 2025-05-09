8cde9f4350c9fbbd4360454a850972ef976b239d
JensTK <jenskaarud@gmail.com>
2025-05-09 20:19:25 +0200

    Add camera settings, change to create backup folders

--- EaW-XML-Replacer/MainWindow.xaml.cs
+++ EaW-XML-Replacer/MainWindow.xaml.cs
@@ -18,9 +18,21 @@ namespace EaW_XML_Replacer;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private const string? TacticalCameraSpaceModeTag = "<TacticalCamera Name=\"Space_Mode\">";
+    private const string? TacticalCameraLandModeTag = "<TacticalCamera Name=\"Land_Mode\">";
+    private const string DistanceMaxTag = "<Distance_Max>";
+    private const string FovMaxTag = "<Fov_Max>";
+    private const string GmcInitialPullbackDistanceTag = "<GMC_InitialPullbackDistance>";
+    private const string SpaceFowRevealRangeTag = "<Space_FOW_Reveal_Range>";
+    private const string LandFowRevealRangeTag = "<Land_FOW_Reveal_Range>";
+    private const string FovDefaultTag = "<Fov_Default>";
+    private const string FovMinTag = "<Fov_Min>";
     private DirectoryInfo? sourceDirectory = null;
-    private DirectoryInfo? targetDirectory = null;
+    private DirectoryInfo? backupDirectory = null;
     private FileInfo[]? xmlFiles = null;
+    private FileInfo? TacticalCamerasFile = null;
+    private FileInfo? GameConstantsFile = null;
+    
     public MainWindow()
     {
         InitializeComponent();
@@ -28,15 +40,14 @@ public partial class MainWindow : Window
     
     private void ToggleEditButtons()
     {
-        bool enable = sourceDirectory != null && targetDirectory != null;
+        bool enable = sourceDirectory != null && backupDirectory != null;
         MultGroundViewButton.IsEnabled = enable;
         MultSpaceViewButton.IsEnabled = enable;
-    }
-
-    private void ToggleEditButtons(bool enabled)
-    {
-        MultGroundViewButton.IsEnabled = enabled;
-        MultSpaceViewButton.IsEnabled = enabled;
+        SetGcCameraHeightButton.IsEnabled = GameConstantsFile != null && backupDirectory != null;
+        SetGroundCameraFovButton.IsEnabled = TacticalCamerasFile != null && backupDirectory != null;
+        SetGroundCameraHeightButton.IsEnabled = TacticalCamerasFile != null && backupDirectory != null;
+        SetSpaceCameraFovButton.IsEnabled = TacticalCamerasFile != null && backupDirectory != null;
+        SetSpaceCameraHeightButton.IsEnabled = TacticalCamerasFile != null && backupDirectory != null;
     }
 
     private void SourceSelectButton_OnClick(object sender, RoutedEventArgs e)
@@ -48,7 +59,21 @@ public partial class MainWindow : Window
             sourceDirectory = new DirectoryInfo(dialog.FolderName);
             //xmlFiles = dir.GetFiles("*.xml", SearchOption.AllDirectories);
             xmlFiles = sourceDirectory.GetFiles("*.xml", SearchOption.AllDirectories);
+            TacticalCamerasFile = xmlFiles.FirstOrDefault(file => file.Name.StartsWith("TacticalCameras", StringComparison.InvariantCultureIgnoreCase));
+            GameConstantsFile = xmlFiles.FirstOrDefault(file => file.Name.StartsWith("GameConstants", StringComparison.InvariantCultureIgnoreCase));
             FileListLabel.Content = "Detected XML files: " + xmlFiles.Length;
+            if (GameConstantsFile is not null)
+            {
+                GcCameraHeightBox.Text = ReadValueInFile(GameConstantsFile, GmcInitialPullbackDistanceTag)?.ToString() ?? "";
+            }
+            if (TacticalCamerasFile is not null)
+            {
+                GroundCameraHeightBox.Text = ReadValueInFile(TacticalCamerasFile, DistanceMaxTag, TacticalCameraLandModeTag)?.ToString() ?? "";
+                SpaceCameraHeightBox.Text = ReadValueInFile(TacticalCamerasFile, DistanceMaxTag, TacticalCameraSpaceModeTag)?.ToString() ?? "";
+                GroundCameraFovBox.Text = ReadValueInFile(TacticalCamerasFile, FovMaxTag, TacticalCameraLandModeTag)?.ToString() ?? "";
+                SpaceCameraFovBox.Text = ReadValueInFile(TacticalCamerasFile, FovMaxTag, TacticalCameraSpaceModeTag)?.ToString() ?? "";
+            }
+            
             /*foreach (var file in xmlFiles)
             {
                 //Console.WriteLine(file.FullName);
@@ -61,80 +86,132 @@ public partial class MainWindow : Window
         ToggleEditButtons();
     }
     
-    private void TargetSelectButton_OnClick(object sender, RoutedEventArgs e)
+    private void BackupSelectButton_OnClick(object sender, RoutedEventArgs e)
     {
         OpenFolderDialog dialog = new OpenFolderDialog();
         if (dialog.ShowDialog() == true)
         {
             TargetTextBox.Content = dialog.FolderName;
-            targetDirectory = new DirectoryInfo(dialog.FolderName);
+            backupDirectory = new DirectoryInfo(dialog.FolderName);
         }
         ToggleEditButtons();
     }
 
     private void MultiplyXmlValues(string xmlStartTag, double multiplier)
     {
-        // replikere mappestrukturen i target
         if (xmlFiles is null || xmlFiles.Length == 0)
         {
             MessageBox.Show("No XML files found in selected folder.");
             return;
         }
 
-        if (sourceDirectory is null || targetDirectory is null)
+        if (sourceDirectory is null || backupDirectory is null)
         {
             throw new Exception("Source and Target are required.");
         }
         
-        int linesUpdated = 0;
         int filesUpdated = 0;
         
         foreach (var file in xmlFiles)
         {
-            if (file.DirectoryName is null)
-            {
-                throw new Exception("Directory not found.");
-            }
-            bool fileUpdated = false;
-            string[] lines = File.ReadAllLines(file.FullName);
-            string[] updatedLines = new string[lines.Length];
-            for (int i = 0; i < lines.Length; i++)
-            {
-                string line = lines[i];
-                string trimmedLine = line.Trim();
-                if (trimmedLine.StartsWith(xmlStartTag))
-                {
-                    string valueString = trimmedLine.Substring(xmlStartTag.Length, trimmedLine.Length - (xmlStartTag.Length * 2) - 1); // ???
-                    // <Space_FOW_Reveal_Range>6000.0</Space_FOW_Reveal_Range>
-                    double value = double.Parse(valueString);
-                    if (value > 0)
-                    {
-                        value *= multiplier;
-                        updatedLines[i] = line.Replace(valueString, value.ToString("0.0"));
-                        linesUpdated++;
-                        fileUpdated = true;
-                    }
-                }
-                else
-                {
-                    updatedLines[i] = line;
-                }
-            }
-            if (fileUpdated)
+            if (SetValueInFile(file, xmlStartTag, d => d * multiplier))
             {
                 filesUpdated++;
-                DirectoryInfo writeDir = Directory.CreateDirectory(file.DirectoryName.Replace(sourceDirectory.FullName, targetDirectory.FullName));
-                File.WriteAllLines(writeDir.FullName + "/" + file.Name, updatedLines);
             }
         }
-        MessageBox.Show($"Updated {linesUpdated} vales in {filesUpdated} files.");
+        MessageBox.Show($"Updated in {filesUpdated} files.");
+    }
+
+    private double? ReadValueInFile(FileInfo file, string xmlStartTag, string? xmlSectionTag = null)
+    {
+        string[] lines = File.ReadAllLines(file.FullName);
+        string? xmlSectionEndTag = xmlSectionTag?.Split(' ').First().Replace("<", "</");
+        bool isInSection = false;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            string trimmedLine = line.Trim();
+            if (xmlSectionTag is not null && trimmedLine.StartsWith(xmlSectionTag))
+            {
+                isInSection = true;
+            }
+            else if (xmlSectionEndTag is not null && trimmedLine.StartsWith(xmlSectionEndTag))
+            {
+                isInSection = false;
+            }
+            if (((xmlSectionTag is not null && isInSection) || xmlSectionTag is null) && trimmedLine.StartsWith(xmlStartTag))
+            {
+                string valueString = trimmedLine.Substring(xmlStartTag.Length, trimmedLine.Length - (xmlStartTag.Length * 2) - 1);
+                return double.Parse(valueString);
+            }
+        }
+        return null;
+    }
+
+    private bool SetValueInFile(FileInfo file, string xmlStartTag, Func<double, double> valueFunc, string? xmlSectionTag = null)
+    {
+        if (sourceDirectory is null || backupDirectory is null)
+        {
+            throw new Exception("Source and Target are required.");
+        }
+        if (file.DirectoryName is null)
+        {
+            throw new Exception("Directory not found.");
+        }
+        bool fileUpdated = false;
+        string[] lines = File.ReadAllLines(file.FullName);
+        string[] updatedLines = new string[lines.Length];
+        string? xmlSectionEndTag = xmlSectionTag?.Split(' ').First().Replace("<", "</");
+        bool isInSection = false;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            string trimmedLine = line.Trim();
+            if (xmlSectionTag is not null && trimmedLine.StartsWith(xmlSectionTag))
+            {
+                isInSection = true;
+            }
+            else if (xmlSectionEndTag is not null && trimmedLine.StartsWith(xmlSectionEndTag))
+            {
+                isInSection = false;
+            }
+            if (((xmlSectionTag is not null && isInSection) || xmlSectionTag is null) && trimmedLine.StartsWith(xmlStartTag))
+            {
+                string valueString = trimmedLine.Substring(xmlStartTag.Length, trimmedLine.Length - (xmlStartTag.Length * 2) - 1); // ???
+                // <Space_FOW_Reveal_Range>6000.0</Space_FOW_Reveal_Range>
+                double value = double.Parse(valueString);
+                if (value > 0)
+                {
+                    value = valueFunc(value);
+                    updatedLines[i] = line.Replace(valueString, value.ToString("0.0"));
+                    //linesUpdated++;
+                    fileUpdated = true;
+                }
+            }
+            else
+            {
+                updatedLines[i] = line;
+            }
+        }
+        if (fileUpdated)
+        {
+            //filesUpdated++;
+            DirectoryInfo backupDir = Directory.CreateDirectory(file.DirectoryName.Replace(sourceDirectory.FullName, backupDirectory.FullName));
+            string copyPath = backupDir.FullName + "/" + file.Name;
+            if (!File.Exists(copyPath))
+            {
+                File.Copy(file.FullName, copyPath, false);
+            }
+            File.WriteAllLines(file.FullName, updatedLines);
+        }
+        return fileUpdated;
     }
 
     private void MultSpaceViewButton_OnClick(object sender, RoutedEventArgs e)
     {
         if (double.TryParse(MultSpaceViewMultiplierBox.Text, out double multValue))
         {
-            MultiplyXmlValues("<Space_FOW_Reveal_Range>", multValue);
+            MultiplyXmlValues(SpaceFowRevealRangeTag, multValue);
         }
         else
         {
@@ -146,11 +223,101 @@ public partial class MainWindow : Window
     {
         if (double.TryParse(MultGroundViewMultiplierBox.Text, out double multValue))
         {
-            MultiplyXmlValues("<Land_FOW_Reveal_Range>", multValue);
+            MultiplyXmlValues(LandFowRevealRangeTag, multValue);
         }
         else
         {
             MessageBox.Show("Invalid mult value.");
+        }
+    }
+
+    private void SetGcCameraHeightButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (GameConstantsFile is null)
+        {
+            throw new Exception("GameConstants file not found.");
+        }
+        if (double.TryParse(GcCameraHeightBox.Text, out double setValue))
+        {
+            SetValueInFile(GameConstantsFile, GmcInitialPullbackDistanceTag, _ => setValue);
+        }
+        else
+        {
+            MessageBox.Show("Invalid value.");
+        }
+    }
+
+    private void SetGroundCameraHeightButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        // <Distance_Max> <TacticalCamera Name="Land_Mode">
+        if (TacticalCamerasFile is null)
+        {
+            throw new Exception("GameConstants file not found.");
+        }
+        if (double.TryParse(GroundCameraHeightBox.Text, out double setValue))
+        {
+            SetValueInFile(TacticalCamerasFile, DistanceMaxTag, _ => setValue, TacticalCameraLandModeTag);
+        }
+        else
+        {
+            MessageBox.Show("Invalid value.");
+        }
+    }
+
+    private void SetGroundCameraFovButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        // Disse er i samme fil men forskjellige sections...
+        // <Fov_Default>65.0</Fov_Default>
+        // <Fov_Max>65.0</Fov_Max>
+        // <Fov_Min>65.0</Fov_Min>
+        if (TacticalCamerasFile is null)
+        {
+            throw new Exception("GameConstants file not found.");
+        }
+        if (double.TryParse(GroundCameraFovBox.Text, out double setValue))
+        {
+            SetValueInFile(TacticalCamerasFile, FovDefaultTag, _ => setValue, TacticalCameraLandModeTag);
+            SetValueInFile(TacticalCamerasFile, FovMaxTag, _ => setValue, TacticalCameraLandModeTag);
+            SetValueInFile(TacticalCamerasFile, FovMinTag, _ => setValue, TacticalCameraLandModeTag);
+        }
+        else
+        {
+            MessageBox.Show("Invalid value.");
+        }
+    }
+
+    private void SetSpaceCameraHeightButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        // <TacticalCamera Name="Space_Mode">
+        if (TacticalCamerasFile is null)
+        {
+            throw new Exception("GameConstants file not found.");
+        }
+        if (double.TryParse(SpaceCameraHeightBox.Text, out double setValue))
+        {
+            SetValueInFile(TacticalCamerasFile, DistanceMaxTag, _ => setValue, TacticalCameraSpaceModeTag);
+        }
+        else
+        {
+            MessageBox.Show("Invalid value.");
+        }
+    }
+
+    private void SetSpaceCameraFovButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (TacticalCamerasFile is null)
+        {
+            throw new Exception("GameConstants file not found.");
+        }
+        if (double.TryParse(SpaceCameraFovBox.Text, out double setValue))
+        {
+            SetValueInFile(TacticalCamerasFile, FovDefaultTag, _ => setValue, TacticalCameraSpaceModeTag);
+            SetValueInFile(TacticalCamerasFile, FovMaxTag, _ => setValue, TacticalCameraSpaceModeTag);
+            SetValueInFile(TacticalCamerasFile, FovMinTag, _ => setValue, TacticalCameraSpaceModeTag);
+        }
+        else
+        {
+            MessageBox.Show("Invalid value.");
         }
     }
 }
